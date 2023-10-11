69a3e7385f5e81d4ab2127198f9f8a5d3f3eab59
iiamsona <sona22hakobyan@gmail.com>
2023-10-11 12:58:02 +0400

    full dialog

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -7,107 +7,147 @@
         </v-card-title>
 
         <v-card-text>
-          <validation-observer v-slot="{ handleSubmit }" ref="observer" slim>
-            <v-form @submit.prevent="handleSubmit(submit)">
-              <validation-provider
-                v-slot="{ errors }"
-                name="title"
-                rules="required|max:255"
-                class="mb-2"
-                tag="div"
-              >
-                <v-text-field
-                  v-model="form.title"
-                  outlined
-                  :counter="255"
-                  :error-messages="errors"
-                  label="Title"
-                />
+          <v-row>
+            <v-col lg="1" md="1">
+              <template>
+                <v-menu rounded="lg" offset-y>
+                  <template #activator="{ attrs, on }">
+                    <v-sheet
+                      rounded="circle"
+                      width="22"
+                      height="22"
+                      :color="form.color ?? defaultColor"
+                      v-bind="attrs"
+                      v-on="on"
+                    />
+                  </template>
+                  <v-list>
+                    <v-list-item v-for="(eColors, index) in eventColors" :key="index" class="px-2">
+                      <v-btn
+                        v-for="(eColor, cIndex) in eColors"
+                        :key="cIndex"
+                        class="d-flex item-center justify-content-center"
+                        icon
+                        x-small
+                        @click="form.color = eColor"
+                      >
+                        <v-sheet
+                          width="20"
+                          height="20"
+                          :color="eColor"
+                          rounded="circle"
+                          :class="{ 'ml-1': cIndex, 'mr-1': !cIndex }"
+                          class="d-flex item-center justify-content-center"
+                        />
+                      </v-btn>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </template>
+            </v-col>
+            <v-col>
+              <validation-provider v-slot="{ handleSubmit }" ref="observer" slim>
+                <v-form @submit.prevent="handleSubmit(submit)">
+                  <validation-provider
+                    v-slot="{ errors }"
+                    name="title"
+                    rules="required|max:255"
+                    class="mb-2"
+                    tag="div"
+                  >
+                    <v-text-field
+                      v-model="form.title"
+                      outlined
+                      :counter="255"
+                      :error-messages="errors"
+                      label="Title"
+                    />
+                  </validation-provider>
+                </v-form>
               </validation-provider>
+            </v-col>
+          </v-row>
+          <validation-provider
+            v-slot="{ errors }"
+            slim
+            name="description"
+            rules="required|max:500"
+            class="mb-2"
+            tag="div"
+          >
+            <v-textarea
+              v-model="form.description"
+              outlined
+              name="Description"
+              label="Description"
+              :counter="500"
+              persistent-hint
+              :error-messages="errors"
+            />
+          </validation-provider>
 
+          <v-row no-gutter>
+            <v-col>
               <validation-provider
                 v-slot="{ errors }"
                 slim
-                name="description"
-                rules="required|max:500"
+                name="start date"
+                rules="required"
                 class="mb-2"
                 tag="div"
               >
-                <v-textarea
-                  v-model="form.description"
-                  outlined
-                  name="Description"
-                  label="Description"
-                  :counter="500"
-                  persistent-hint
-                  :error-messages="errors"
-                />
+                <v-datetime-picker
+                  v-model="event.start"
+                  :text-field-props="{ outlined: true, prependIcon: 'mdi-calendar', errorMessages: errors }"
+                  :date-picker-props="{noTitle: true}"
+                  label="Start time"
+                  ok-text="Save"
+                  clear-text="Reset"
+                >
+                  <template #dateIcon>
+                    <v-icon>mdi-calendar</v-icon>
+                  </template>
+                  <template #timeIcon>
+                    <v-icon>mdi-clock-outline</v-icon>
+                  </template>
+                </v-datetime-picker>
               </validation-provider>
-
-              <v-row no-gutter>
-                <v-col>
-                  <validation-provider
-                    v-slot="{ errors }"
-                    slim
-                    name="start date"
-                    rules="required"
-                    class="mb-2"
-                    tag="div"
-                  >
-                    <v-datetime-picker
-                      v-model="event.start"
-                      :text-field-props="{ outlined: true, prependIcon: 'mdi-calendar', errorMessages: errors }"
-                      :date-picker-props="{noTitle: true}"
-                      label="Start time"
-                      ok-text="Save"
-                      clear-text="Reset"
-                    >
-                      <template #dateIcon>
-                        <v-icon>mdi-calendar</v-icon>
-                      </template>
-                      <template #timeIcon>
-                        <v-icon>mdi-clock-outline</v-icon>
-                      </template>
-                    </v-datetime-picker>
-                  </validation-provider>
-                </v-col>
-                <v-col>
-                  <validation-provider
-                    v-slot="{ errors }"
-                    slim
-                    name="start date"
-                    rules="required"
-                    class="mb-2"
-                    tag="div"
-                  >
-                    <v-datetime-picker
-                      v-model="event.end"
-                      :text-field-props="{ outlined: true, errorMessages: errors }"
-                      :date-picker-props="{ noTitle: true }"
-                      label="End time"
-                      ok-text="Save"
-                      clear-text="Reset"
-                    >
-                      <template #dateIcon>
-                        <v-icon>mdi-calendar</v-icon>
-                      </template>
-                      <template #timeIcon>
-                        <v-icon>mdi-clock-outline</v-icon>
-                      </template>
-                    </v-datetime-picker>
-                  </validation-provider>
-                </v-col>
-              </v-row>
-              <div class="text-right mt-4">
-                <v-btn depressed @click="newEvent = false">
-                  Close
-                </v-btn>
-                <v-btn color="primary" depressed type="submit">
-                  Save
-                </v-btn>
-              </div>
-            </v-form>
-          </validation-observer>
+            </v-col>
+            <v-col>
+              <validation-provider
+                v-slot="{ errors }"
+                slim
+                name="start date"
+                rules="required"
+                class="mb-2"
+                tag="div"
+              >
+                <v-datetime-picker
+                  v-model="event.end"
+                  :text-field-props="{ outlined: true, errorMessages: errors }"
+                  :date-picker-props="{ noTitle: true }"
+                  label="End time"
+                  ok-text="Save"
+                  clear-text="Reset"
+                >
+                  <template #dateIcon>
+                    <v-icon>mdi-calendar</v-icon>
+                  </template>
+                  <template #timeIcon>
+                    <v-icon>mdi-clock-outline</v-icon>
+                  </template>
+                </v-datetime-picker>
+              </validation-provider>
+            </v-col>
+          </v-row>
+          <div class="text-right mt-4">
+            <v-btn depressed @click="newEvent = false">
+              Close
+            </v-btn>
+            <v-btn color="primary" depressed type="submit">
+              Save
+            </v-btn>
+          </div>
         </v-card-text>
       </v-card>
     </v-dialog>
@@ -120,15 +160,27 @@
 </template>
 
 <script>
+import { ValidationProvider } from 'vee-validate'
+
 export default {
   name: 'Calendar',
+  components: { ValidationProvider },
   data () {
     return {
       newEvent: false,
       form: {
         title: null,
-        description: null
+        description: null,
+        color: '#BE8CFF'
       },
+      defaultColor: '#BE8CFF',
+      eventColors: [
+        ['#BE8CFF', '#FFC94A'],
+        ['#4E7AB2', '#B6C258'],
+        ['#A9A9A9', '#F27B3F'],
+        ['#7B638D', '#8AC4FF'],
+        ['#C15374', '#000000']
+      ],
       event: {
         start: null,
         end: null
@@ -147,7 +199,6 @@ export default {
         this.form.user_id = this.user.id
         await this.createEvent(this.form)
       }
-
       this.dialog = false
     }
   }
